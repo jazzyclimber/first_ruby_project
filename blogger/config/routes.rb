67e222abc3816a70fe4ebc32a61c8911fea8fcfc
jazzyclimber <jazzyclimber@gmail.com>
2019-05-01 15:40:02 -0500

    added some basic authentication protocal

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
+  resources :authors
+
   resources :articles do
     resources :comments
   end
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
